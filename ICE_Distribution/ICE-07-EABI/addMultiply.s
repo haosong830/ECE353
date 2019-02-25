cc0d3bda8bcb9e557a18d3ec2ed3b763dc043ddb
kwilson33 <kwilson33@ad.engr.wisc.edu>
2019-02-25 09:17:49 -0600

    ice 07

--- ICE_Distribution/ICE-07-EABI/addMultiply.s
+++ ICE_Distribution/ICE-07-EABI/addMultiply.s
@@ -46,68 +46,75 @@ addMultiply PROC
 	; R2 = Array Address 2
 	; R3 = Array Address 3
 	
-    ; Validate Address Parameters (nonzero)
+	; Validate Address Parameters (nonzero)
+	CMP R0, #0
+	MOVEQ R0, #-1
+	BEQ invalid_parameter
 	
+	CMP R1, #0
+	MOVEQ R0, #-1
+	BEQ invalid_parameter
 	
+	CMP R2, #0
+	MOVEQ R0, #-1
+	BEQ invalid_parameter
 	
-	; Get 5th parameter
-	LDR R4, [SP, 0]
-	; Validate Size Parameter (greater than zero)
-	
-	
-	
-	
-	
-	
-	
-	
+	CMP R3, #0
+	MOVEQ R0, #-1
+	BEQ invalid_parameter
 	
 	; Must save anything besides R0-R3. Caller must save R0-R3
 	; R4 will hold the array size
-	; R5 will hold Array0[i]
+	; R5 will hold Array0[i] and the sum of Array0 and Array1 and the product of the sum and Array2
 	; R6 will hold Array1[i]
 	; R7 will hold Array2[i]
-	; R8 will hold i
-	; R9 will hold Array0[i] + Array1[i] and (Array0[i] + Array1[i]) * Array2[i]
-	PUSH {R4-R9}
+	PUSH {R4-R7}
+	
+	; Get 5th parameter
+	LDR R4, [SP, #0]
+	; Validate Size Parameter (greater than zero)
+	CMP R4, #0
+	MOVLE R0, #-1
+	BEQ array_loop_end
 	
 	
-    
+	
     ; For each index in the arrays, compute  
     ; Array3[i] = (Array0[i] + Array1[i]) * Array2[i]
-	
-	; initialize i
-	MOV R8, #0
-	
 array_loop_begin
-	; compare i to array size
-	CMP R8, R4
+	; compare array size to 0
+	CMP R4, #0
 	BEQ array_loop_end
 	
 	; since each entry is 8 bits, only have to offset 
 	; by 1 byte each time. No shifting of i necessary
 	; load array values. Signed values
-	LDRSB R5, [R0, R8]
-	LDRSB R6, [R1, R8]
-	LDRSB R7, [R2, R8]
+	LDRSB R5, [R0], #1
+	LDRSB R6, [R1], #1
+	LDRSB R7, [R2], #1
 	
 	;(Array0[i] + Array1[i])
-	ADD R9, R5, R6
+	ADD R5, R5, R6
 	;(Array0[i] + Array1[i]) * Array2[i]
-	MUL R9, R9, R7
+	MUL R5, R5, R7
 	
 	; store result in Array3[i]
-	STRSB R9, [R3, R8]
+	STR R5, [R3], #1
 	
-	; increment index
-	ADD R8, R8, #1
-	
+	; decrement size of array
+	SUB R4, R4, #1
 	; keep looping
 	B array_loop_begin
 array_loop_end
-	
+	; valid return value
+	MOV R0, #0
+	; if any parameters were invalid, R0 won't be set to 0
+invalid_parameter
+
     ; Restore registers saved to the stack
-	POP {R4-R9}
+	POP {R4-R8}
+	
+	; valid return value
 	
     ; Return from the loop
 	BX LR
